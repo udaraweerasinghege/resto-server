1b519299282b9d2821f4086d9c516d48a7258f51
Udara Weerasinghege <udara.weera@gmail.com>
2018-01-18 22:22:10 -0500

    update resto route

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../db')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  // make db requests
+/* GET restos listing. */
+router.get('/', (req, res, next) => {
+  db.query('SELECT * FROM restaurants', null, (err, res) => {
+    if (err) {
+      return next(err)
+    }
+    res.send(res.rows[0])
+  })
 });
 
 module.exports = router;
