9bb6b67984bf8195231bc4137b8be2b360c17651
Meagan Pau <meagan.pau@crowdlinker.com>
2018-08-31 10:54:54 -0400

    dotenv and request packages, yelp api call

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
 
@@ -24,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/api', index);
 
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: graphqlSchema }));
 if (process.env.NODE_ENV === 'development') {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,50 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.status(200).send('Success!')
+});
+
+router.get('/yelp', function(req, res, next) {
+    const { id } = req.query;
+
+    request({
+        method: 'GET',
+        uri: `https://api.yelp.com/v3/businesses/${id}`,
+        headers: {
+            'Authorization': `Bearer ${process.env.YELP_API_KEY}`,
+        },
+    },
+
+    function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            res.send(body);
+        } else {
+            res.send(error);
+        }
+    });
+});
+
+router.get('/yelpsearch', function(req, res, next) {
+    const { term, long, lat } = req.query;
+
+    request({
+        method: 'GET',
+        uri: `https://api.yelp.com/v3/autocomplete?text=${term}&latitude=${lat}&longitude=${long}`,
+        headers: {
+            'Authorization': `Bearer ${process.env.YELP_API_KEY}`,
+        },
+    },
+
+    function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            res.send(body);
+        } else {
+            res.send(error);
+        }
+    });
 });
 
 module.exports = router;
