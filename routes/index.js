1c72c0dc843ef7342b918432f6c6acfd854580ef
Meagan Pau <meagan.pau@crowdlinker.com>
2019-02-21 18:34:21 -0500

    route for yelp business

--- routes/index.js
+++ routes/index.js
@@ -27,12 +27,32 @@ router.get('/yelp', function(req, res, next) {
     });
 });
 
-router.get('/yelpsearch', function(req, res, next) {
+router.get('/yelp-search', function(req, res, next) {
     const { term, long, lat } = req.query;
 
     request({
         method: 'GET',
         uri: `https://api.yelp.com/v3/autocomplete?text=${term}&latitude=${lat}&longitude=${long}`,
+        headers: {
+            'Authorization': `Bearer ${process.env.YELP_API_KEY}`,
+        },
+    },
+
+    function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            res.send(body);
+        } else {
+            res.send(error);
+        }
+    });
+});
+
+router.get('/yelp-business', function(req, res, next) {
+    const { id } = req.query;
+
+    request({
+        method: 'GET',
+        uri: `https://api.yelp.com/v3/businesses/${id}`,
         headers: {
             'Authorization': `Bearer ${process.env.YELP_API_KEY}`,
         },
